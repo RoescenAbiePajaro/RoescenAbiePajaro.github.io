8a7a9021354a6a2475926cbed58d7a327e6dece4
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2024-10-07 21:36:23 -0500

    Initial Commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,34 @@
+<footer
+    class="py-20 sm:py-32 bg-black border-t border-solid border-orange-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+>
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Contact Me.
+    </p>
+    <div class="flex flex-col gap-4 items-center justify-center rounded-lg">
+         <div
+            class="flex items-center gap-4 sm:gap-6 text-2xl sm:text-3xl md:text-4xl mx-auto"
+        >
+          
+        </div>
+
+        <p><b class="pr-2">Email</b>abiepajaro@gmail.com</p>
+        <p>
+        </p>
+        <p>
+            <b class="pr-2">Contact Number:</b>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a target="_blank" class="text-orange-400"
+                >09299716043<sup class=""
+                    ><span class="text-xs scale-75 pl-0.5"
+                        ><i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        /></span
+                    ></sup
+                ></a
+            >
+        </p>
+
+        <p class="mx-auto text-sm opacity-70">Youtube.freecodecamp</p>
+        <span class="">https://www.youtube.com/watch?v=-2UjwQzxvBQ&t=159s</span>
+    </div>
+</footer>
